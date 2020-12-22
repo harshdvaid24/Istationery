b722ad8278803f480095e1dee12f7ba69c42940b
harshdvaid24 <harshdvaid24@gmail.com>
2020-12-22 22:33:55 +0530

    ios app renamed

--- android/app/src/main/java/com/istationery/MainApplication.java
+++ android/app/src/main/java/com/istationery/MainApplication.java
@@ -1,4 +1,4 @@
-package com.magento_mobile_app;
+package com.istationery;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.magento_mobile_app.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.istationery.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
